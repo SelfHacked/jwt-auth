0014e477252d4d6c533da8da65d56ee6ae1dda02
Andrew Terpolovsky <andrew.terpolovsky@gmail.com>
2020-02-27 20:16:18 +0200

    Small code refactoring.

--- jwt_auth/service_authorization.py
+++ jwt_auth/service_authorization.py
@@ -7,10 +7,9 @@ class ServiceRequestAuth(AuthBase):
     """A custom class for authenticating requests with other micro-services."""
 
     def __call__(self, req):
-        """Custom implementation for authentication.
-        """
+        """Custom implementation for authentication."""
         token = settings.JWT_AUTH['SERVICE_SECRET_TOKEN']
 
         # Set the token in the request
-        req.headers["Token"] = token
+        req.headers['Token'] = token
         return req
